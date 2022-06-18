6a470d07e7b081379bc5e598c5fc449d8e24ed3e
Zach <zgleason37@gmail.com>
2022-06-17 21:49:41 -0700

    sprint[3]

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
               </h3>
               <p>
               A highly motivated, creative, and intuitive full-stack software engineer who has a huge passion for computers and anything technology related. 
-              Anything and everything that can be classified as technology excites me! Currently living in Sonoma County, Caliornia.
+              Anything and everything that can be classified as technology excites me! Currently living in Sonoma County, California.
               </p>
             </div>
             <div className="my_skills">
